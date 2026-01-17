6d756b044016463f74cd42f85c33d8969c444aa0
panaiotuzunov <panaiot.uzunov@gmail.com>
2026-01-17 13:55:50 +0200

    minor change in test handler

--- handlers.go
+++ handlers.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (cfg *apiConfig) Test(writer http.ResponseWriter, req *http.Request) {
-	result := fmt.Sprint("<html><body><h1>The server works!</h1></body></html>")
 	writer.Header().Add("Content-Type", "text/html; charset=utf-8")
 	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte(result))
+	writer.Write([]byte("<html><body><h1>The server works!</h1></body></html>"))
 }
