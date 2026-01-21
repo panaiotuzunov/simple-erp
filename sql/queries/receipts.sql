094ae9e9329c5a3dfe0a40b0cd678150c76b87b0
panaiotuzunov <panaiot.uzunov@gmail.com>
2026-01-21 16:50:43 +0200

    modify GetAllReceiptsUnion to return negative values for exit receipts

--- sql/queries/receipts.sql
+++ sql/queries/receipts.sql
@@ -8,8 +8,15 @@ FROM entrance_receipts
 UNION ALL
 
 SELECT 
-    *,
-    (gross - tare)::NUMERIC(12,3) AS net,
+    id,
+    created_at,
+    updated_at,
+    truck_reg,
+    trailer_reg,
+    (gross * -1)::NUMERIC(12,3) AS gross,
+    (tare * -1)::NUMERIC(12,3) AS tare,
+    grain_type,
+    ((gross - tare) * -1)::NUMERIC(12,3) AS net,
     'exit' AS receipt_type 
 FROM exit_receipts
 ORDER BY id;
